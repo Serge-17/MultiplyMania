309787c0509527c0c666a50e1ba892c7f8e54b17
Serge-17 <sergei.eliseev7@icloud.com>
2025-04-07 13:24:36 +0300

    Initial Commit

---
+++ MultiplyMania/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MultiplyMania
+//
+//  Created by Serge Eliseev on 07.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MultiplyMania/MultiplyManiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplyManiaApp.swift
+//  MultiplyMania
+//
+//  Created by Serge Eliseev on 07.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct MultiplyManiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
